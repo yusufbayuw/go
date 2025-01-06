2b2b3ff7a6d2476d066ab77843a765068ce78f16
Yusuf <yusufbayu31.com>
2025-01-07 02:17:00 +0700

    baselink redirect

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\GoController;
 use Illuminate\Support\Facades\Redirect;
 
 Route::get('/', function () {
-    return Redirect::away('https://tbu.ac.id');
+    return Redirect::away(env('BASE_LINK'));
 });
 Route::get('/{urlx}', [GoController::class, 'gotolink']);
