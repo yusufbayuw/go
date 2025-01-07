455bc957b89c8ceee6603745efcda2614a0ddaca
Yusuf Bayu Wicaksono <yusuf.wicaksono@tarunabakti.or.id>
2025-01-07 14:30:18 +0700

    base urls

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\GoController;
 use Illuminate\Support\Facades\Redirect;
 
 Route::get('/', function () {
-    return Redirect::away(env('BASE_LINK'));
+    return Redirect::away(config('base_urls.base_home'));
 });
 Route::get('/{urlx}', [GoController::class, 'gotolink']);
